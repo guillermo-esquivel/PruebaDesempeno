802e89993bd7d02df7aa3bd2de8673a8734f3c50
triniii <zaldivaralcocert05@gmail.com>
2026-03-04 02:40:59 -0600

    Carga de imágenes de Pokemon y Zelda

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,5 @@
+<div class="col-lg-4">
+    <img src="{{ asset($imagen) }}" class="img-fluid">
+    <h4>{{ $nombre }}</h4>
+    <p>${{ $precio }}</p>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,9 @@ use App\Http\Controllers\PokemonController;
 use App\Http\Controllers\ZeldaController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/mario', [MarioController::class, 'index']);
-Route::get('/zelda', [ZeldaController::class, 'index']);
-Route::get('/pokemon', [PokemonController::class, 'index']);
+Route::get('/mario', [MarioController::class, 'index'])->name('mario');
+Route::get('/zelda', [ZeldaController::class, 'index'])->name('zelda');
+Route::get('/pokemon', [PokemonController::class, 'index'])->name('pokemon');
 Route::get('/', function () {
     return view('welcome');
 });
